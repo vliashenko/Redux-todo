be0b776e21aa50253e275304e8cdaa282e2e6ec9
Viacheslav Liashenko <87814580+vliashenko@users.noreply.github.com>
2022-08-04 11:53:34 +0300

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,40 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import { 
+    persistStore, 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import todoReduser from "./todoSlice"
 
-export default configureStore({
-    reducer: {
-        todos: todoReduser,
-    }
+const rootReducer = combineReducers({
+    todos: todoReduser,
+})
+
+const persistConfig = {
+    key: "root",
+    storage,
+    blacklist: ["filter"]
+}
+
+const persistedReducer = persistReducer( persistConfig, rootReducer )
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+
+export const persistor = persistStore(store)
+
+export default store;
